ff51449ca9afe47bb8cffa3812655eead35182c1
HackRicken <hackricken@gmail.com>
2021-12-03 17:05:10 +0800

    Update Sitetime.js

--- js/Sitetime.js
+++ js/Sitetime.js
@@ -23,6 +23,6 @@ function siteTime() {
      var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours);
      var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes);
      var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes * minutes) / seconds);
-         document.getElementById("sitetime").innerHTML = "已运行 " +   diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
+         document.getElementById("sitetime").innerHTML = "<br>已运行 " +   diffDays + " 天 " + diffHours + " 小时 " + diffMinutes + " 分钟 " + diffSeconds + " 秒";
          }  
          siteTime();
